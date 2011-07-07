b6201d1491702bcdf7e2fec97bcc013d39f686f3
Dimitri Eberhardt Prado <dprado@e3c.com.br>
2011-07-07 15:04:52 -0300

    Extend Braille driver to support a 40 cell layout

--- arch/arm/mach-omap2/pugboard-metec.c
+++ arch/arm/mach-omap2/pugboard-metec.c
@@ -40,9 +40,13 @@
 #define VOLTAGE 0xFF
 #define DEVICE_NAME "braille"
 
-#define LINE_LENGTH 20
+#define NUMBER_OF_LINES 2
 
 #undef METEC_DEBUG
+
+#if NUMBER_OF_LINES > 2 
+#error Invalid line length
+#endif
 
 static int major = 0;
 static int dev_open = false;
@@ -51,29 +55,47 @@ struct task_struct* read_thread;
 struct input_dev *input_dev;
 
 static void write_pattern(char* patt);
-
-static const unsigned int keymap[20] = 
-{
-    KEY_F17,
-    KEY_F18,
-    KEY_F19,
-    KEY_F20,
-    KEY_F13,
-    KEY_F14,
-    KEY_F15,
-    KEY_F16,
-    KEY_F9,
-    KEY_F10,
-    KEY_F11,
-    KEY_F12,
-    KEY_F5,
-    KEY_F6,
-    KEY_F7,
-    KEY_F8,
-    KEY_F1,
-    KEY_F2,
-    KEY_F3,
-    KEY_F4,
+static cursor_map[] = {
+    16,
+    17,
+    18,
+    19,
+    12,
+    13,
+    14,
+    15,
+    8,
+    9,
+    10,
+    11,
+    4,
+    5,
+    6,
+    7,
+    0,
+    1,
+    2,
+    3,
+    36,
+    37,
+    38,
+    39,
+    32,
+    33,
+    34,
+    35,
+    28,
+    29,
+    30,
+    31,
+    24,
+    25,
+    26,
+    27,
+    20,
+    21,
+    22,
+    23
 };
 
 static int metec_open(struct inode *inode, struct file *filp)
@@ -92,77 +114,78 @@ static int metec_release(struct inode *inode, struct file *filp)
 
 void build_pattern(char* buf, char* patt)
 {
-#if LINE_LENGTH < 20
-#error Invalid line length
-#endif
 #ifdef METEC_DEBUG
     int i = 0;
     printk("IN: ");
-    for(i = 0; i < 20; i++)
+    for(i = 0; i < NUMBER_OF_LINES * 20; i++)
     {
         printk(" %02x", buf[i]);
     }
     printk("\n");
+#else
+    int i = 0;
 #endif
+    for(i = 0; i < NUMBER_OF_LINES; i++)
+    {
+        printk("building line %d\n", i);
+        patt[0 + (24*(NUMBER_OF_LINES-i-1))] = 0;
+        patt[1 + (24*(NUMBER_OF_LINES-i-1))] = (buf[19 + (20*i)] & 0x40) << 1 | (buf[18 + (20*i)] & 0x40) >> 1 | (buf[17 + (20*i)] & 0x40) >> 3 | (buf[16 + (20*i)] & 0x40) >> 5;
+        patt[2 + (24*(NUMBER_OF_LINES-i-1))] = (buf[15 + (20*i)] & 0x40) << 1| (buf[14 + (20*i)] & 0x40) >> 1| (buf[13 + (20*i)] & 0x40) >> 3| (buf[12 + (20*i)] & 0x40) >> 5;
+        patt[3 + (24*(NUMBER_OF_LINES-i-1))] = (buf[11 + (20*i)] & 0x40) << 1| (buf[10 + (20*i)] & 0x40) >> 1| (buf[9 + (20*i)] & 0x40) >> 3| (buf[8 + (20*i)] & 0x40) >> 5;
+        patt[4 + (24*(NUMBER_OF_LINES-i-1))] = (buf[7 + (20*i)] & 0x40) << 1| (buf[6 + (20*i)] & 0x40) >> 1| (buf[5 + (20*i)] & 0x40) >> 3| (buf[4 + (20*i)] & 0x40) >> 5;
+        patt[5 + (24*(NUMBER_OF_LINES-i-1))] = (buf[3 + (20*i)] & 0x40) << 1| (buf[2 + (20*i)] & 0x40) >> 1| (buf[1 + (20*i)] & 0x40) >> 3| (buf[0 + (20*i)] & 0x40) >> 5;
 
-    patt[0] = 0;
-    patt[1] = (buf[19] & 0x40) << 1 | (buf[18] & 0x40) >> 1 | (buf[17] & 0x40) >> 3 | (buf[16] & 0x40) >> 5;
-    patt[2] = (buf[15] & 0x40) << 1| (buf[14] & 0x40) >> 1| (buf[13] & 0x40) >> 3| (buf[12] & 0x40) >> 5;
-    patt[3] = (buf[11] & 0x40) << 1| (buf[10] & 0x40) >> 1| (buf[9] & 0x40) >> 3| (buf[8] & 0x40) >> 5;
-    patt[4] = (buf[7] & 0x40) << 1| (buf[6] & 0x40) >> 1| (buf[5] & 0x40) >> 3| (buf[4] & 0x40) >> 5;
-    patt[5] = (buf[3] & 0x40) << 1| (buf[2] & 0x40) >> 1| (buf[1] & 0x40) >> 3| (buf[0] & 0x40) >> 5;
+        patt[1 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[19 + (20*i)] & 0x80) >> 1| (buf[18 + (20*i)] & 0x80) >> 3 | (buf[17 + (20*i)] & 0x80) >> 5 | (buf[16 + (20*i)] & 0x80) >> 7;
+        patt[2 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[15 + (20*i)] & 0x80) >> 1| (buf[14 + (20*i)] & 0x80) >> 3| (buf[13 + (20*i)] & 0x80) >> 5| (buf[12 + (20*i)] & 0x80) >> 7;
+        patt[3 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[11 + (20*i)] & 0x80) >> 1| (buf[10 + (20*i)] & 0x80) >> 3| (buf[9 + (20*i)] & 0x80) >> 5| (buf[8 + (20*i)] & 0x80) >> 7;
+        patt[4 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[7 + (20*i)] & 0x80) >> 1| (buf[6 + (20*i)] & 0x80) >> 3| (buf[5 + (20*i)] & 0x80) >> 5| (buf[4 + (20*i)] & 0x80) >> 7;
+        patt[5 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[3 + (20*i)] & 0x80) >> 1| (buf[2 + (20*i)] & 0x80) >> 3| (buf[1 + (20*i)] & 0x80) >> 5| (buf[0 + (20*i)] & 0x80) >> 7;
 
-    patt[1] |= (buf[19] & 0x80) >> 1| (buf[18] & 0x80) >> 3 | (buf[17] & 0x80) >> 5 | (buf[16] & 0x80) >> 7;
-    patt[2] |= (buf[15] & 0x80) >> 1| (buf[14] & 0x80) >> 3| (buf[13] & 0x80) >> 5| (buf[12] & 0x80) >> 7;
-    patt[3] |= (buf[11] & 0x80) >> 1| (buf[10] & 0x80) >> 3| (buf[9] & 0x80) >> 5| (buf[8] & 0x80) >> 7;
-    patt[4] |= (buf[7] & 0x80) >> 1| (buf[6] & 0x80) >> 3| (buf[5] & 0x80) >> 5| (buf[4] & 0x80) >> 7;
-    patt[5] |= (buf[3] & 0x80) >> 1| (buf[2] & 0x80) >> 3| (buf[1] & 0x80) >> 5| (buf[0] & 0x80) >> 7;
+        patt[6 + (24*(NUMBER_OF_LINES-i-1))] = 0;
 
-    patt[6] = 0;
+        patt[7 + (24*(NUMBER_OF_LINES-i-1))] = (buf[19 + (20*i)] & 0x4) << 5 | (buf[18 + (20*i)] & 0x4) << 3| (buf[17 + (20*i)] & 0x4) << 1| (buf[16 + (20*i)] & 0x4) >> 1;
+        patt[8 + (24*(NUMBER_OF_LINES-i-1))] = (buf[15 + (20*i)] & 0x4) << 5| (buf[14 + (20*i)] & 0x4) << 3| (buf[13 + (20*i)] & 0x4) << 1| (buf[12 + (20*i)] & 0x4) >> 1;
+        patt[9 + (24*(NUMBER_OF_LINES-i-1))] = (buf[11 + (20*i)] & 0x4) << 5| (buf[10 + (20*i)] & 0x4) << 3| (buf[9 + (20*i)] & 0x4) << 1| (buf[8 + (20*i)] & 0x4) >> 1;
+        patt[10 + (24*(NUMBER_OF_LINES-i-1))] = (buf[7 + (20*i)] & 0x4) << 5| (buf[6 + (20*i)] & 0x4) << 3| (buf[5 + (20*i)] & 0x4) << 1| (buf[4 + (20*i)] & 0x4) >> 1;
+        patt[11 + (24*(NUMBER_OF_LINES-i-1))] = (buf[3 + (20*i)] & 0x4) << 5| (buf[2 + (20*i)] & 0x4) << 3| (buf[1 + (20*i)] & 0x4) << 1| (buf[0 + (20*i)] & 0x4) >> 1;
 
-    patt[7] = (buf[19] & 0x4) << 5 | (buf[18] & 0x4) << 3| (buf[17] & 0x4) << 1| (buf[16] & 0x4) >> 1;
-    patt[8] = (buf[15] & 0x4) << 5| (buf[14] & 0x4) << 3| (buf[13] & 0x4) << 1| (buf[12] & 0x4) >> 1;
-    patt[9] = (buf[11] & 0x4) << 5| (buf[10] & 0x4) << 3| (buf[9] & 0x4) << 1| (buf[8] & 0x4) >> 1;
-    patt[10] = (buf[7] & 0x4) << 5| (buf[6] & 0x4) << 3| (buf[5] & 0x4) << 1| (buf[4] & 0x4) >> 1;
-    patt[11] = (buf[3] & 0x4) << 5| (buf[2] & 0x4) << 3| (buf[1] & 0x4) << 1| (buf[0] & 0x4) >> 1;
+        patt[7 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[19 + (20*i)] & 0x20) << 1 | (buf[18 + (20*i)] & 0x20) >> 1| (buf[17 + (20*i)] & 0x20) >> 3| (buf[16 + (20*i)] & 0x20) >> 5;
+        patt[8 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[15 + (20*i)] & 0x20) << 1| (buf[14 + (20*i)] & 0x20) >> 1| (buf[13 + (20*i)] & 0x20) >> 3| (buf[12 + (20*i)] & 0x20) >> 5;
+        patt[9 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[11 + (20*i)] & 0x20) << 1| (buf[10 + (20*i)] & 0x20) >> 1| (buf[9 + (20*i)] & 0x20) >> 3| (buf[8 + (20*i)] & 0x20) >> 5;
+        patt[10 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[7 + (20*i)] & 0x20) << 1| (buf[6 + (20*i)] & 0x20) >> 1| (buf[5 + (20*i)] & 0x20) >> 3| (buf[4 + (20*i)] & 0x20) >> 5;
+        patt[11 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[3 + (20*i)] & 0x20) << 1| (buf[2 + (20*i)] & 0x20) >> 1| (buf[1 + (20*i)] & 0x20) >> 3| (buf[0 + (20*i)] & 0x20) >> 5;
 
-    patt[7] |= (buf[19] & 0x20) << 1 | (buf[18] & 0x20) >> 1| (buf[17] & 0x20) >> 3| (buf[16] & 0x20) >> 5;
-    patt[8] |= (buf[15] & 0x20) << 1| (buf[14] & 0x20) >> 1| (buf[13] & 0x20) >> 3| (buf[12] & 0x20) >> 5;
-    patt[9] |= (buf[11] & 0x20) << 1| (buf[10] & 0x20) >> 1| (buf[9] & 0x20) >> 3| (buf[8] & 0x20) >> 5;
-    patt[10] |= (buf[7] & 0x20) << 1| (buf[6] & 0x20) >> 1| (buf[5] & 0x20) >> 3| (buf[4] & 0x20) >> 5;
-    patt[11] |= (buf[3] & 0x20) << 1| (buf[2] & 0x20) >> 1| (buf[1] & 0x20) >> 3| (buf[0] & 0x20) >> 5;
+        patt[12 + (24*(NUMBER_OF_LINES-i-1))] = 0;
 
-    patt[12] = 0;
+        patt[13 + (24*(NUMBER_OF_LINES-i-1))] = (buf[19 + (20*i)] & 0x2) << 6 | (buf[18 + (20*i)] & 0x2) << 4 | (buf[17 + (20*i)] & 0x2) << 2 | (buf[16 + (20*i)] & 0x2);
+        patt[14 + (24*(NUMBER_OF_LINES-i-1))] = (buf[15 + (20*i)] & 0x2) << 6| (buf[14 + (20*i)] & 0x2) << 4| (buf[13 + (20*i)] & 0x2) << 2| (buf[12 + (20*i)] & 0x2);
+        patt[15 + (24*(NUMBER_OF_LINES-i-1))] = (buf[11 + (20*i)] & 0x2) << 6| (buf[10 + (20*i)] & 0x2) << 4| (buf[9 + (20*i)] & 0x2) << 2| (buf[8 + (20*i)] & 0x2);
+        patt[16 + (24*(NUMBER_OF_LINES-i-1))] = (buf[7 + (20*i)] & 0x2) << 6| (buf[6 + (20*i)] & 0x2) << 4| (buf[5 + (20*i)] & 0x2) << 2| (buf[4 + (20*i)] & 0x2);
+        patt[17 + (24*(NUMBER_OF_LINES-i-1))] = (buf[3 + (20*i)] & 0x2) << 6| (buf[2 + (20*i)] & 0x2) << 4| (buf[1 + (20*i)] & 0x2) << 2| (buf[0 + (20*i)] & 0x2);
 
-    patt[13] = (buf[19] & 0x2) << 6 | (buf[18] & 0x2) << 4 | (buf[17] & 0x2) << 2 | (buf[16] & 0x2);
-    patt[14] = (buf[15] & 0x2) << 6| (buf[14] & 0x2) << 4| (buf[13] & 0x2) << 2| (buf[12] & 0x2);
-    patt[15] = (buf[11] & 0x2) << 6| (buf[10] & 0x2) << 4| (buf[9] & 0x2) << 2| (buf[8] & 0x2);
-    patt[16] = (buf[7] & 0x2) << 6| (buf[6] & 0x2) << 4| (buf[5] & 0x2) << 2| (buf[4] & 0x2);
-    patt[17] = (buf[3] & 0x2) << 6| (buf[2] & 0x2) << 4| (buf[1] & 0x2) << 2| (buf[0] & 0x2);
+        patt[13 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[19 + (20*i)] & 0x10) << 2 | (buf[18 + (20*i)] & 0x10) | (buf[17 + (20*i)] & 0x10) >> 2 | (buf[16 + (20*i)] & 0x10) >> 4;
+        patt[14 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[15 + (20*i)] & 0x10) << 2 | (buf[14 + (20*i)] & 0x10) | (buf[13 + (20*i)] & 0x10) >> 2| (buf[12 + (20*i)] & 0x10) >> 4;
+        patt[15 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[11 + (20*i)] & 0x10) << 2 | (buf[10 + (20*i)] & 0x10) | (buf[9 + (20*i)] & 0x10) >> 2| (buf[8 + (20*i)] & 0x10) >> 4;
+        patt[16 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[7 + (20*i)] & 0x10) << 2 | (buf[6 + (20*i)] & 0x10) | (buf[5 + (20*i)] & 0x10) >> 2| (buf[4 + (20*i)] & 0x10) >> 4;
+        patt[17 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[3 + (20*i)] & 0x10) << 2 | (buf[2 + (20*i)] & 0x10) | (buf[1 + (20*i)] & 0x10) >> 2| (buf[0 + (20*i)] & 0x10) >> 4;
 
-    patt[13] |= (buf[19] & 0x10) << 2 | (buf[18] & 0x10) | (buf[17] & 0x10) >> 2 | (buf[16] & 0x10) >> 4;
-    patt[14] |= (buf[15] & 0x10) << 2 | (buf[14] & 0x10) | (buf[13] & 0x10) >> 2| (buf[12] & 0x10) >> 4;
-    patt[15] |= (buf[11] & 0x10) << 2 | (buf[10] & 0x10) | (buf[9] & 0x10) >> 2| (buf[8] & 0x10) >> 4;
-    patt[16] |= (buf[7] & 0x10) << 2 | (buf[6] & 0x10) | (buf[5] & 0x10) >> 2| (buf[4] & 0x10) >> 4;
-    patt[17] |= (buf[3] & 0x10) << 2 | (buf[2] & 0x10) | (buf[1] & 0x10) >> 2| (buf[0] & 0x10) >> 4;
+        patt[18 + (24*(NUMBER_OF_LINES-i-1))] = VOLTAGE;
 
-    patt[18] = VOLTAGE;
+        patt[19 + (24*(NUMBER_OF_LINES-i-1))] = (buf[19 + (20*i)] & 0x01) << 7| (buf[18 + (20*i)] & 0x01) << 5| (buf[17 + (20*i)] & 0x01) << 3| (buf[16 + (20*i)] & 0x01) << 1;
+        patt[20 + (24*(NUMBER_OF_LINES-i-1))] = (buf[15 + (20*i)] & 0x01) << 7| (buf[14 + (20*i)] & 0x01) << 5| (buf[13 + (20*i)] & 0x01) << 3| (buf[12 + (20*i)] & 0x01) << 1;
+        patt[21 + (24*(NUMBER_OF_LINES-i-1))] = (buf[11 + (20*i)] & 0x01) << 7| (buf[10 + (20*i)] & 0x01) << 5| (buf[9 + (20*i)] & 0x01) << 3| (buf[8 + (20*i)] & 0x01) << 1;
+        patt[22 + (24*(NUMBER_OF_LINES-i-1))] = (buf[7 + (20*i)] & 0x01) << 7| (buf[6 + (20*i)] & 0x01) << 5| (buf[5 + (20*i)] & 0x01) << 3| (buf[4 + (20*i)] & 0x01) << 1;
+        patt[23 + (24*(NUMBER_OF_LINES-i-1))] = (buf[3 + (20*i)] & 0x01) << 7| (buf[2 + (20*i)] & 0x01) << 5| (buf[1 + (20*i)] & 0x01) << 3| (buf[0 + (20*i)] & 0x01) << 1;
 
-    patt[19] = (buf[19] & 0x01) << 7| (buf[18] & 0x01) << 5| (buf[17] & 0x01) << 3| (buf[16] & 0x01) << 1;
-    patt[20] = (buf[15] & 0x01) << 7| (buf[14] & 0x01) << 5| (buf[13] & 0x01) << 3| (buf[12] & 0x01) << 1;
-    patt[21] = (buf[11] & 0x01) << 7| (buf[10] & 0x01) << 5| (buf[9] & 0x01) << 3| (buf[8] & 0x01) << 1;
-    patt[22] = (buf[7] & 0x01) << 7| (buf[6] & 0x01) << 5| (buf[5] & 0x01) << 3| (buf[4] & 0x01) << 1;
-    patt[23] = (buf[3] & 0x01) << 7| (buf[2] & 0x01) << 5| (buf[1] & 0x01) << 3| (buf[0] & 0x01) << 1;
-
-    patt[19] |= (buf[19] & 0x08) << 3| (buf[18] & 0x08) << 1| (buf[17] & 0x08) >> 1| (buf[16] & 0x08) >> 3;
-    patt[20] |= (buf[15] & 0x08) << 3| (buf[14] & 0x08) << 1| (buf[13] & 0x08) >> 1| (buf[12] & 0x08) >> 3;
-    patt[21] |= (buf[11] & 0x08) << 3| (buf[10] & 0x08) << 1| (buf[9] & 0x08) >> 1| (buf[8] & 0x08) >> 3;
-    patt[22] |= (buf[7] & 0x08) << 3| (buf[6] & 0x08) << 1| (buf[5] & 0x08) >> 1| (buf[4] & 0x08) >> 3;
-    patt[23] |= (buf[3] & 0x08) << 3| (buf[2] & 0x08) << 1| (buf[1] & 0x08) >> 1| (buf[0] & 0x08) >> 3;
-
+        patt[19 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[19 + (20*i)] & 0x08) << 3| (buf[18 + (20*i)] & 0x08) << 1| (buf[17 + (20*i)] & 0x08) >> 1| (buf[16 + (20*i)] & 0x08) >> 3;
+        patt[20 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[15 + (20*i)] & 0x08) << 3| (buf[14 + (20*i)] & 0x08) << 1| (buf[13 + (20*i)] & 0x08) >> 1| (buf[12 + (20*i)] & 0x08) >> 3;
+        patt[21 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[11 + (20*i)] & 0x08) << 3| (buf[10 + (20*i)] & 0x08) << 1| (buf[9 + (20*i)] & 0x08) >> 1| (buf[8 + (20*i)] & 0x08) >> 3;
+        patt[22 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[7 + (20*i)] & 0x08) << 3| (buf[6 + (20*i)] & 0x08) << 1| (buf[5 + (20*i)] & 0x08) >> 1| (buf[4 + (20*i)] & 0x08) >> 3;
+        patt[23 + (24*(NUMBER_OF_LINES-i-1))] |= (buf[3 + (20*i)] & 0x08) << 3| (buf[2 + (20*i)] & 0x08) << 1| (buf[1 + (20*i)] & 0x08) >> 1| (buf[0 + (20*i)] & 0x08) >> 3;
+    }
 #ifdef METEC_DEBUG
     printk("OUT: ");
-    for(i = 0; i < 24; i++)
+    for(i = 0; i < NUMBER_OF_LINES * 24; i++)
     {
         printk(" %02x", patt[i]);
     }
@@ -175,12 +198,12 @@ ssize_t metec_write(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
     if(!dev_open)
         return -EBUSY;
-    char patt[LINE_LENGTH+4] = {0}; 
-    char buffer[LINE_LENGTH] = {0};
-    memcpy(buffer, buf, (count>LINE_LENGTH?LINE_LENGTH:count));
+    char patt[NUMBER_OF_LINES * 24] = {0}; 
+    char buffer[NUMBER_OF_LINES * 20] = {0};
+    memcpy(buffer, buf, (count>(NUMBER_OF_LINES * 20)?NUMBER_OF_LINES * 20:count));
     build_pattern(buffer, patt);  
     write_pattern(patt);
-    return (count<=LINE_LENGTH?count:LINE_LENGTH); 
+    return (count<=(NUMBER_OF_LINES * 20)?count:(NUMBER_OF_LINES * 20)); 
 }
 
 struct file_operations metec_fops = {
@@ -200,7 +223,8 @@ static void write_pattern(char* patt)
     spin_lock_irqsave(&metec_lock, flags);
 
     gpio_set_value(METEC_STROBE, 1);
-    for(i = 0; i < 24; i++)
+    udelay(10);
+    for(i = 0; i < NUMBER_OF_LINES * 24; i++)
     {
         for(j = 7; j >= 0; j--)
         {
@@ -212,17 +236,16 @@ static void write_pattern(char* patt)
         }
     }
     gpio_set_value(METEC_STROBE, 0);
-
     spin_unlock_irqrestore(&metec_lock, flags);
 }
 
 static int read_keys(void* data)
 {
-    uint32_t state = 0;
-    uint32_t front_keys = 0;
-    uint32_t last_front_keys = 0;
-    uint32_t cursor_keys = 0;
-    uint32_t last_cursor_keys = 0;
+    uint64_t state = 0;
+    uint64_t front_keys = 0;
+    uint64_t last_front_keys = 0;
+    uint64_t cursor_keys = 0;
+    uint64_t last_cursor_keys = 0;
     uint32_t i = 0;
     uint32_t j = 0;
     unsigned long flags;
@@ -233,7 +256,7 @@ static int read_keys(void* data)
         spin_lock_irqsave(&metec_lock, flags);
         gpio_set_value(METEC_STROBE, 0);
         udelay(10);
-        for(i = 0; i < 20; i++)
+        for(i = 0; i < NUMBER_OF_LINES*20; i++)
         {
             state = 0;
             for(j = 0; j < 2; j++)
@@ -247,19 +270,19 @@ static int read_keys(void* data)
             } 
             if(state & 1) 
             {
-                cursor_keys |= 1 << i;
+                cursor_keys |= (uint64_t)1 << i;
             }
             else
             {
-                cursor_keys &= ~ (1<< i);
+                cursor_keys &= ~ ((uint64_t)1<< i);
             }
             if(state & 2) 
             {
-                front_keys |= 1 << i;
+                front_keys |= (uint64_t)1 << i;
             }
             else
             {
-                front_keys &= ~ (1<< i);
+                front_keys &= ~ ((uint64_t)1<< i);
             }
         }
         udelay(10);
@@ -271,7 +294,7 @@ static int read_keys(void* data)
         if(front_keys != last_front_keys)
         {
     #ifdef METEC_DEBUG
-            printk("METEC frontkeys changed from %x to %x\n", last_front_keys, front_keys);
+            printk("METEC frontkeys changed from %llx to %llx\n", last_front_keys, front_keys);
     #endif
             last_front_keys = front_keys;
         }
@@ -280,17 +303,19 @@ static int read_keys(void* data)
         {
             uint32_t k = 0;
     #ifdef METEC_DEBUG
-            printk("METEC cursorkeys changed from %x to %x\n", last_cursor_keys, cursor_keys);
+            printk("METEC cursorkeys changed from %llx to %llx\n", last_cursor_keys, cursor_keys);
     #endif
             /* lets report every difference, only on cursor keys */
-            uint32_t diff_map = last_cursor_keys ^ cursor_keys;
-            for(k = 0; k < 20; k++)
+            uint64_t diff_map = last_cursor_keys ^ cursor_keys;
+            for(k = 0; k < NUMBER_OF_LINES*20; k++)
             {
-                if(diff_map & (1 << k))
+                if(diff_map & ((uint64_t)1 << k))
                 {
-                    input_event(input_dev, EV_MSC, MSC_SCAN, k);
-                    input_report_key(input_dev, keymap[k], (cursor_keys & (1 << k))?0:1);
-                    input_sync(input_dev);
+                    // report only key press
+                    if(!(cursor_keys & ((uint64_t)1 << k)))
+                    {
+                        input_event(input_dev, EV_MSC, MSC_RAW, cursor_map[k]);
+                    }
                 }
             }
             last_cursor_keys = cursor_keys;
@@ -303,8 +328,6 @@ static int read_keys(void* data)
 static int metec_probe(struct platform_device *pdev)
 {
     int err = 0;
-    unsigned short *keycodes;
-
     if((err = gpio_request(METEC_STROBE, "metec STROBE")) < 0)
     {
         printk("Error requesting RS %d\n", err);
@@ -345,22 +368,15 @@ static int metec_probe(struct platform_device *pdev)
     gpio_set_value(METEC_CLK, 0);
     mdelay(1);
     
-    char patt[] = { "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" };
+    char patt[48] = { 0 };
     write_pattern(patt);
 
-    keycodes = kzalloc(20 * sizeof(*keycodes), GFP_KERNEL);
     input_dev = input_allocate_device();
 
     input_dev->name     = "metec-keys";
     input_dev->id.bustype   = BUS_HOST;
-    input_dev->evbit[0] = BIT_MASK(EV_KEY); 
-    input_dev->keycode  = keycodes;
-    input_dev->keycodesize  = sizeof(unsigned int);
-    input_dev->keycodemax   = ARRAY_SIZE(keymap);
-
-    memcpy(input_dev->keycode, keymap, sizeof(keymap));
-
-    input_set_capability(input_dev, EV_MSC, MSC_SCAN);
+    input_dev->evbit[0] = BIT_MASK(EV_MSC); 
+    input_dev->mscbit[0] = BIT(MSC_RAW);
 
     err = input_register_device(input_dev);
     if (err)
@@ -371,8 +387,8 @@ static int metec_probe(struct platform_device *pdev)
 	return 0;
 
 err_free_mem:
+    printk("Error registering device\n");
     input_free_device(input_dev);
-    kfree(keycodes);
     return err;
 }
 
