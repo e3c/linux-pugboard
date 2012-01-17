d5bdb0f8aa4e42e9a5543e475f53193fb10f88be
Dimitri Eberhardt Prado <dprado@e3c.com.br>
2012-01-17 18:08:44 -0200

    remove old code and eliminate warnings.

--- arch/arm/mach-omap2/pugboard-lcd-i2c.c
+++ arch/arm/mach-omap2/pugboard-lcd-i2c.c
@@ -37,10 +37,6 @@
 
 #undef LCD_DEBUG
 
-static spinlock_t lcd_lock;;
-static void put_char(char cmd);
-static void put_cmd(char cmd);
-
 struct i2c_client* lcd_client = NULL;
 
 static struct char_map_list{
@@ -52,11 +48,6 @@ static struct char_map_list{
 
 static int major = 0;
 static int dev_open = false;
-
-static int cur_line = 0;
-static int cur_col = 0;
-static char second_line[40];
-
 
 static ssize_t char_map_show(struct device *dev,
                   struct device_attribute *attr,
@@ -102,20 +93,24 @@ static ssize_t char_map_store(struct device *dev,
                    struct device_attribute *attr,
                    const char *buf, size_t n)
 {
-    char* read_key = buf;
+    const char* read_key = buf;
     char* read_map = strchr(buf, ',') + 1;
-    if(read_map == 1)
+    unsigned char* key = (char*)kmalloc((strlen(read_key) - strlen(read_map))/2, GFP_KERNEL);
+    unsigned char* map = (char*)kmalloc(9, GFP_KERNEL);
+    struct char_map_list *tmp;
+    struct list_head *q, *pos;
+    struct char_map_list* tmp_item = (struct char_map_list*)kmalloc(sizeof(struct char_map_list), GFP_KERNEL);
+    unsigned long long hex = 0;
+    unsigned int alt;
+    int i = 0;
+
+    if((int)read_map == 1)
     {
             printk(KERN_EMERG "lcdpug: wrong key/map format, missing comma\n");
             return -EINVAL;
     }
-    unsigned char* key = (char*)kmalloc((strlen(read_key) - strlen(read_map))/2, GFP_KERNEL);
-    unsigned char* map = (char*)kmalloc(9, GFP_KERNEL);
     printk(KERN_EMERG "Malloced map %p\n", map);
     printk(KERN_EMERG "Malloced key %p\n", key);
-    unsigned long long hex = 0;
-    unsigned int alt;
-    int i = 0;
     if (sscanf(read_map+i, "%llx,%x", &hex,&alt) != 2) {
         if(sscanf(read_map+i, ",%x", &alt) != 1){
             printk(KERN_EMERG "lcdpug: wrong map format, must be 8*HH\n");
@@ -137,8 +132,6 @@ static ssize_t char_map_store(struct device *dev,
     strncpy(key, read_key, read_map - read_key - 1);
 
     /* remove old key */
-    struct char_map_list *tmp;
-    struct list_head *q, *pos;
     list_for_each_safe(pos, q, &char_map.list)
     {
         tmp = list_entry(pos, struct char_map_list, list);
@@ -153,7 +146,6 @@ static ssize_t char_map_store(struct device *dev,
         }
     }
 
-    struct char_map_list* tmp_item = (struct char_map_list*)kmalloc(sizeof(struct char_map_list), GFP_KERNEL);
     printk(KERN_EMERG "Malloced item %p\n", tmp_item);
     tmp_item->key = key;
     tmp_item->map = map;
@@ -181,57 +173,6 @@ static int lcd_release(struct inode *inode, struct file *filp)
     printk("lcd close\n");
     dev_open = false;
     return 0;
-}
-
-static void scroll()
-{
-    int i = 0;
-#ifdef LCD_DEBUG
-    printk("scroll 2nd line %s\n", second_line);
-#endif
-    put_cmd(0x80);
-    cur_line= 0;
-    for(i = 0; i < 40; i++)
-    {
-        put_char(second_line[i]);
-    } 
-    cur_col = 0;
-}
-
-static void new_line()
-{
-    int i = 0;
-#ifdef LCD_DEBUG
-    printk("newline\n");
-#endif
-    if(cur_line == 1)
-    {
-        scroll();
-    }
-    put_cmd(0xC0);
-    cur_line = 1;
-    for(i = 0; i < 40; i++)
-    {
-        put_char(0x20);
-    }
-    put_cmd(0xC0);
-    cur_col = 0;
-}
-
-static void carriage_return()
-{
-#ifdef LCD_DEBUG
-    printk("carriage return\n");
-#endif
-    if(cur_line == 0)
-    {
-        put_cmd(0x80);
-    }
-    else
-    {
-        put_cmd(0xC0);
-    }
-    cur_col = 0;
 }
 
 static int translate(char* in)
@@ -282,17 +223,26 @@ static int translate(char* in)
 static ssize_t lcd_write(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
     int i = 0;
+    int res = 0;
+    char tmp[80];
     printk("lcd write\n");
-#if 0
-    while((i < 80) && (i < count))
+    while((i < 39) && (i < count - 1))
     {
+#if 0
         int res = translate(&buf[i]);
 #ifdef LCD_DEBUG
         printk("translate returned %d\n", res);
 #endif
         i += res;
-    } 
 #endif
+        tmp[i*2] = 0xc0;
+        tmp[(i*2)+1] = buf[i];     
+        i++;
+    } 
+    tmp[i*2] = 0x40;
+    tmp[(i*2)+1] = buf[i];
+    res = i2c_master_send(lcd_client, tmp, (i*2) + 2);
+    printk(KERN_EMERG "write res %d\n", res);
     return count; 
 }
 
@@ -302,236 +252,15 @@ static struct file_operations lcd_fops = {
   release: lcd_release
 };
 
-
-static void lcd_init_write(unsigned char cmd) 
-{ 
-#if 0
-    gpio_set_value(LCD_RS, 0);
-    gpio_set_value(LCD_RW, 0);
-    mdelay(1);
-
-    gpio_set_value(LCD_DB7, ((cmd & 0x80)?1:0));
-    gpio_set_value(LCD_DB6, ((cmd & 0x40)?1:0));
-    gpio_set_value(LCD_DB5, ((cmd & 0x20)?1:0));
-    gpio_set_value(LCD_DB4, ((cmd & 0x10)?1:0));
-
-    gpio_set_value(LCD_E, 1);
-    mdelay(1);
-    gpio_set_value(LCD_E, 0);
-#endif
-} 
-
-static void write_byte(char cmd, char rs)
-{
-#if 0
-    spin_lock(&lcd_lock);
-
-    gpio_set_value(LCD_RS, rs);
-    gpio_set_value(LCD_RW, 0);
-    mdelay(1);
-
-    gpio_set_value(LCD_DB7, (cmd & 0x80)?1:0);
-    gpio_set_value(LCD_DB6, (cmd & 0x40)?1:0);
-    gpio_set_value(LCD_DB5, (cmd & 0x20)?1:0);
-    gpio_set_value(LCD_DB4, (cmd & 0x10)?1:0);
-
-    gpio_set_value(LCD_E, 1);
-    mdelay(1);
-    gpio_set_value(LCD_E, 0);
-    mdelay(1);
-
-    gpio_set_value(LCD_DB7, (cmd & 0x08)?1:0);
-    gpio_set_value(LCD_DB6, (cmd & 0x04)?1:0);
-    gpio_set_value(LCD_DB5, (cmd & 0x02)?1:0);
-    gpio_set_value(LCD_DB4, (cmd & 0x1)?1:0);
-    gpio_set_value(LCD_E, 1);
-    mdelay(1);
-    gpio_set_value(LCD_E, 0);
-    
-    spin_unlock(&lcd_lock);
-#endif
-}
-
-static void put_char(char cmd)
-{
-#ifdef LCD_DEBUG
-    printk("put %x\n", cmd);
-#endif
-#if 0
-    write_byte(cmd, 1);
-    if(cur_line == 1)
-    {
-        second_line[cur_col] = cmd;
-    }
-    cur_col++;
-#endif
-}
-
-static void put_cmd(char cmd)
-{
-#ifdef LCD_DEBUG
-    printk("cmd %x\n", cmd);
-#endif
-    write_byte(cmd, 0);
-}
-
 static int lcd_i2c_probe(struct i2c_client *client, const struct i2c_device_id *idp)
 {
-    int err = 0;
-    printk("LCD probe!!!!!!!!!!!!!!!!!!!!!!!!!! Client is %x\n", client);
-    lcd_client = client;
-
-#if 0
-    if((err = gpio_request(LCD_RS, "lcd RS")) < 0)
-    {
-        printk("Error requesting RS %d\n", err);
-        return err;
-    }
-    if((err = gpio_request(LCD_RW, "lcd RW")) < 0)
-    {
-        printk("Error requesting RW %d\n", err);
-        return err;
-    }
-    if((err = gpio_request(LCD_E, "lcd E")) < 0)
-    {
-        printk("Error requesting E %d\n", err);
-        return err;
-    }
-    if((err = gpio_request(LCD_DB4, "lcd DB4")) < 0)
-    {
-        printk("Error requesting DB4 %d\n", err);
-        return err;
-    }
-    if((err = gpio_request(LCD_DB5, "lcd DB5")) < 0)
-    {
-        printk("Error requesting DB5 %d\n", err);
-        return err;
-    }
-    if((err = gpio_request(LCD_DB6, "lcd DB6")) < 0)
-    {
-        printk("Error requesting DB6 %d\n", err);
-        return err;
-    }
-    if((err = gpio_request(LCD_DB7, "lcd DB7")) < 0)
-    {
-        printk("Error requesting DB7 %d\n", err);
-        return err;
-    }
-    mdelay(1);
-    gpio_direction_output(LCD_RS, 0);
-    gpio_direction_output(LCD_RW, 0);
-    gpio_direction_output(LCD_E, 0);
-    gpio_direction_output(LCD_DB4, 0);
-    gpio_direction_output(LCD_DB5, 0);
-    gpio_direction_output(LCD_DB6, 0);
-    gpio_direction_output(LCD_DB7, 0);
-    mdelay(50);
-
-    /* magic init sequence for 4bit comm. */
-    lcd_init_write(0x30);
-    mdelay(10);
-    lcd_init_write(0x30);
-    mdelay(5);
-    lcd_init_write(0x30);
-    mdelay(5);
-    lcd_init_write(0x20);
-
-    /* 4bit 2lines 5x7 font */
-    put_cmd(0x28);
-
-  /* turn off display */
-    put_cmd(0x08);
-    /* clear display */
-    put_cmd(0x01);
-    mdelay(5);
-    /* increment display */
-    put_cmd(0x06);
-    /* turn on display, cursor */
-    put_cmd(0x0E);
-
-    /* temporary hack to add ã and ç to CGRAM */
-    put_cmd(0x40);
-    put_char(0x00);
-    put_char(0x00);
-    put_char(0x00);
-    put_char(0x00);
-    put_char(0x00);
-    put_char(0x00);
-    put_char(0x00);
-    put_char(0x00);
-
-    /* ç */
-    put_char(0x00);
-    put_char(0x00);
-    put_char(0x0E);
-    put_char(0x10);
-    put_char(0x11);
-    put_char(0x0E);
-    put_char(0x04);
-    put_char(0x0C);
-
-    /* ã */
-    put_char(0x0D);
-    put_char(0x12);
-    put_char(0x00);
-    put_char(0x0E);
-    put_char(0x01);
-    put_char(0x0F);
-    put_char(0x11);
-    put_char(0x0F);
-
-    /* á */
-    put_char(0x02);
-    put_char(0x04);
-    put_char(0x00);
-    put_char(0x0E);
-    put_char(0x01);
-    put_char(0x0F);
-    put_char(0x11);
-    put_char(0x0F);
-
-    /* é */
-    put_char(0x02);
-    put_char(0x04);
-    put_char(0x00);
-    put_char(0x0E);
-    put_char(0x11);
-    put_char(0x1F);
-    put_char(0x10);
-    put_char(0x0E);
-
-    /* ê */
-    put_char(0x04);
-    put_char(0x0A);
-    put_char(0x00);
-    put_char(0x0E);
-    put_char(0x11);
-    put_char(0x1F);
-    put_char(0x10);
-    put_char(0x0E);
-
-    /* ó */
-    put_char(0x00);
-    put_char(0x02);
-    put_char(0x04);
-    put_char(0x00);
-    put_char(0x0E);
-    put_char(0x11);
-    put_char(0x11);
-    put_char(0x0E);
-
-    /* go home */
-    put_cmd(0x02);
-#endif
     int res = i2c_master_send(client, "\x00\x38", 2);
-    printk("master_send returned %d\n", res);
     mdelay(10);
     res = i2c_master_send(client, "\x00\x39", 2);
-    printk("master_send returned %d\n", res);
     mdelay(10);
     res = i2c_master_send(client, "\x80\x14\x80\x78\x80\x5e\x80\x6d\x80\x0c\x80\x01\x00\x06", 14);
-    printk("master_send returned %d\n", res);
     mdelay(10);
+    lcd_client = client;
 	return 0;
 }
 
@@ -582,7 +311,8 @@ static struct device* lcdpug_device;
 
 static int __init lcd_i2c_drv_init(void)
 {
-    memset(second_line, 0x20, sizeof(second_line));
+    int res = 0;
+
     major = register_chrdev(0, DEVICE_NAME, &lcd_fops);
     printk("LCD I2C ST7036 registered major %d\n", major);
 
@@ -590,11 +320,11 @@ static int __init lcd_i2c_drv_init(void)
     lcdpug_device = device_create(lcdpug_class, NULL, MKDEV(major, 0), NULL, "lcdpug");
 
     INIT_LIST_HEAD( &char_map.list);
-    device_create_file(lcdpug_device, &dev_attr_cmap_add);
-    device_create_file(lcdpug_device, &dev_attr_cmap_del);
-    device_create_file(lcdpug_device, &dev_attr_cmap_show);
+    res = device_create_file(lcdpug_device, &dev_attr_cmap_add);
+    res = device_create_file(lcdpug_device, &dev_attr_cmap_del);
+    res = device_create_file(lcdpug_device, &dev_attr_cmap_show);
 
-	int res = i2c_add_driver(&lcd_i2c_driver);
+	res = i2c_add_driver(&lcd_i2c_driver);
     printk("LCD I2C add driver returned %x\n", res);
     return 0;
 }
